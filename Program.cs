9a1127594c9afed7c9249d79ba7dae68b8e7a8c2
internetbird <saharoni@gmail.com>
2025-05-29 10:34:23 +0300

    Day 6

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AOC2019
         {
             IPuzzleSolver solver = new Day6PuzzleSolver();
 
-            var solution = solver.SolvePuzzlePart1();
+            var solution = solver.SolvePuzzlePart2();
             Console.WriteLine($"The solution to the puzzle is: {solution}");
 
             Console.ReadKey();
--- PuzzleSolvers/Day6PuzzleSolver.cs
+++ PuzzleSolvers/Day6PuzzleSolver.cs
@@ -11,15 +11,68 @@ namespace AOC2019.PuzzleSolvers
     {
         public string SolvePuzzlePart1()
         {
+            Dictionary<string, string> dictionary = GetOrbitsDictionary();
+            int totalOrbits = 0;
+            foreach (var key in dictionary.Keys)
+            {
+                string current = key;
+                while (current != "COM")
+                {
+                    totalOrbits++;
+                    current = dictionary[current];
+                }
+            }
+
+
+            return totalOrbits.ToString();
+        }
+
+        private static Dictionary<string, string> GetOrbitsDictionary()
+        {
             string[] inputLines = InputFilesHelper.GetInputFileLines("day6.txt");
 
 
-            return string.Empty;
+            var dictionary = new Dictionary<string, string>();
+            foreach (var line in inputLines)
+            {
+                var parts = line.Split(')');
+                if (parts.Length == 2)
+                {
+                    dictionary[parts[1]] = parts[0];
+                }
+            }
+
+            return dictionary;
         }
 
         public string SolvePuzzlePart2()
         {
-            throw new NotImplementedException();
+            var dictionary = GetOrbitsDictionary();
+
+            var youPath = new List<string>();
+            var sanPath = new List<string>();
+            string current = "YOU";
+            while (current != "COM")
+            {
+                youPath.Add(current);
+                current = dictionary[current];
+            }
+            current = "SAN";
+            while (current != "COM")
+            {
+                sanPath.Add(current);
+                current = dictionary[current];
+            }
+            youPath.Reverse();
+            sanPath.Reverse();
+            int commonIndex = 0;
+            while (commonIndex < youPath.Count && commonIndex < sanPath.Count && youPath[commonIndex] == sanPath[commonIndex])
+            {
+                commonIndex++;
+            }
+            // The number of transfers is the total length of both paths minus twice the common length minus 2 (because we don't count the common nodes themselves).
+            int transfers = (youPath.Count - commonIndex) + (sanPath.Count - commonIndex) - 2;
+            return transfers.ToString();
         }
     }
 }
